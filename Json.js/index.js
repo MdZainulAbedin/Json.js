3943b54abc23e75d857d9007a55bc34adbbd98b9
MdZainulAbedin <108911270+MdZainulAbedin@users.noreply.github.com>
2022-08-05 21:48:40 +0530

    Add files via upload

---
+++ Json.js/index.js
@@ -0,0 +1,38 @@
+var number1 = 13;
+var number2 = 7;
+console.log(number1 +  number2);
+
+
+// 1. Data types in Javascript
+// Numbers
+var num1 = 112;
+var num2 = 12.4;
+
+
+// 2. String
+var str1 = "This is a string"
+var str2 ='This is also a string';
+
+// 3. objects
+var marks = {
+    Asad: 12,
+    Ahad: 13,
+    Azra: 14,
+}
+
+console.log(marks);
+
+// 4. Booleans
+var a = true;
+var b = flase;
+console.log(a,b);
+
+// 5. var und = undefined;
+
+var und;
+console.log(und);
+
+var n = null;
+console.log(n);
+
+
